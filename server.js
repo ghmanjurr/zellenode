26ce0beee29b92f92a6ad1e5a8e1d4cd3c228526
ghmanjurr <manjurr.ghalib@gmail.com>
2021-04-04 13:46:58 -0700

    third commit

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 // server.js
 
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+
+//Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+//All your handlers here...
 
 app.listen(3000, function(){
     console.log('listening on 3000')
@@ -10,4 +17,8 @@ app.listen(3000, function(){
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
     //Note: __dirname is the current directory you're in
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
 })
